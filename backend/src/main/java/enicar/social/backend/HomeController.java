5c3baeea70f429cb549ce800d5e568d67d04efb5
ghaythbenabid <ghaythbenabid0@gmail.com>
2024-02-03 15:57:32 +0100

    initial commit

---
+++ backend/src/main/java/enicar/social/backend/HomeController.java
@@ -0,0 +1,17 @@
+package enicar.social.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+
+    //first endpoint
+    @GetMapping("/")
+    public String home() {
+        return "ENICAR Social Media Application Backend";
+    }
+}

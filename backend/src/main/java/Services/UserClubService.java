05c7a9dbd98b73f9db8ea911e1c3f17e864de990
yassinelamouchi <ylamouchi2001@gmail.com>
2024-03-15 15:07:45 +0100

    UserClub first commit

--- backend/src/main/java/Services/UserClubService.java
+++ backend/src/main/java/Services/UserClubService.java
@@ -1,8 +1,57 @@
 package Services;
 
+import Models.Club;
+import Models.User;
+import Models.UserClub;
+import Repositories.UserClubRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class UserClubService {
+    private final UserClubRepository userClubRepo;
+    @Autowired
+    public UserClubService(UserClubRepository userClubRepo){this.userClubRepo = userClubRepo ;}
+
+    public String CreateUserClubInfo(UserClub userClub){
+        userClubRepo.save(userClub);
+        return "UserCLub Created Successfully ";
+
+    }
+
+    //hna ya gaith
+
+
+    public UserClub UpdateUserClubInfo(long userClubID , UserClub userClub){
+        UserClub existingUserClub = userClubRepo.findById(userClubID).get() ;
+        existingUserClub.setUser(UserClub.getUser()) ;
+        existingUserClub.setClub(UserClub.getClub());
+        existingUserClub.setDate(userClub.getDate());
+
+        return userClubRepo.save(existingUserClub) ;
+    }
+
+    public UserClub getUserClubInfo(long userClubID){
+        return userClubRepo.findById(userClubID).orElse(null);
+    }
+
+    public String DeleteUserClubInfo(long userClubID){
+        userClubRepo.deleteById(userClubID);
+        return "UserClub Deleted Successfully ";
+    }
+
+    public List<UserClub> getALLUserClubInfo(){return userClubRepo.findAll();}
+
+
+
+
+
+
     // Your service logic goes here
 }
+

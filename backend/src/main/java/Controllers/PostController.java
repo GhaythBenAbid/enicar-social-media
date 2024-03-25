eedce44dfa171c5786a32792aec057c5ad2bf8af
yassinelamouchi <ylamouchi2001@gmail.com>
2024-03-25 11:52:34 +0100

    post crud commit

--- backend/src/main/java/Controllers/PostController.java
+++ backend/src/main/java/Controllers/PostController.java
@@ -1,10 +1,67 @@
 package Controllers;
 
+import Models.Event;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import Models.Post;
+
+import Services.PostService;
+
+import Utils.ApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
 
 @RestController
-@RequestMapping("/post")
+@RequestMapping("/api/post")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class PostController {
-    // Your controller logic goes here
+    @Autowired
+    private PostService postService;
+
+    @PostMapping
+    public ResponseEntity  createPost(@RequestBody Post post){
+        try {
+            Post newPost = this.postService.CreatePost(post);
+            return new ResponseEntity(newPost, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity(new ApiResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage()), HttpStatus.BAD_REQUEST);
+        }
+    }
+    @PutMapping("/{postID}")
+    public ResponseEntity<Object> updatePost(@PathVariable("postID") int postID,@RequestBody Post post){
+        this.postService.UpdatePost(postID,post);
+
+        HashMap<String, Object> response = new HashMap<>();
+        response.put("message", "Post Updated Successfully");
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+    @GetMapping
+    public ResponseEntity<List<Post>> getALLPostDetails(){
+        return  new ResponseEntity<>(this.postService.getALLPostInfo(),HttpStatus.OK) ;}
+
+    @GetMapping("/{postID}")
+    public  ResponseEntity<Object> getPostDetails(@PathVariable("postID") int postID) {
+        Models.Post post = postService.getPostInfo(postID).orElse(null);
+        if (post != null) {
+            return new ResponseEntity<>(post, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(new ApiResponse(HttpStatus.NOT_FOUND.value(), "Post not found with id: " + postID), HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+    @DeleteMapping ("/{postID}")
+    public ResponseEntity<Object> DeletePost(@PathVariable("postID") Long postID) {
+        postService.DeletePost(postID);
+        return new ResponseEntity<>(new ApiResponse(HttpStatus.OK.value(), "Post deleted successfully"), HttpStatus.OK);
+    }
+
+
 }
+
--- backend/src/main/java/Services/PostService.java
+++ backend/src/main/java/Services/PostService.java
@@ -1,8 +1,76 @@
 package Services;
 
-import org.springframework.stereotype.Service;
 
+import Models.Event;
+import Models.Post;
+import Models.User;
+import Repositories.PostRepository;
+import Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import Repositories.UserRepository;
+import Utils.JWTUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import java.util.List;
 @Service
 public class PostService {
-    // Your service logic goes here
+
+    private final PostRepository postRepo;
+    private final UserRepository userRepo;
+    @Autowired
+    public PostService(PostRepository postRepo, UserRepository userRepo) {
+        this.postRepo = postRepo;
+        this.userRepo = userRepo;
+    }
+
+    @Autowired
+    private StorageService storageService;
+
+    public Post CreatePost(Post post){postRepo.save(post);
+        return post;
+    }
+
+
+    public Post UpdatePost(long postID , Post post){
+        Post existingPost = postRepo.findById(postID).get() ;
+        existingPost.setTitle(post.getTitle()); ;
+        existingPost.setContent(post.getContent());
+        existingPost.setAuthor(post.getAuthor());
+        existingPost.setDate(post.getDate());
+        existingPost.setTags(post.getTags());
+        existingPost.setVisibility(post.getVisibility());
+        return postRepo.save(existingPost) ;
+    }
+    public Post getPost(long postID){
+        return postRepo.findById(postID).orElse(null);
+    }
+    public void DeletePost(long postID){
+        postRepo.deleteById(postID);
+
+    }
+
+    public List<Post> getALLPostInfo() {return postRepo.findAll();}
+    public Optional<Post> getPostInfo(long postID) {return postRepo.findById(postID);}
+
+
+
+
+
+
+
+
+
 }
